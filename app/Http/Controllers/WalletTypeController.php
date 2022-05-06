aa2a4703b677e50dee076fe2aadb16199242821f
David Adokuru <45779326+adokuru@users.noreply.github.com>
2022-05-06 15:51:02 +0100

    Withdrawal

---
+++ app/Http/Controllers/WalletTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WalletType;
+use Illuminate\Http\Request;
+
+class WalletTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WalletType  $walletType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WalletType $walletType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\WalletType  $walletType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WalletType $walletType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\WalletType  $walletType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WalletType $walletType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WalletType  $walletType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WalletType $walletType)
+    {
+        //
+    }
+}
