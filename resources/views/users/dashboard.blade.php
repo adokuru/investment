b0a4c0ac6a917b3456c914996763d67a1b502141
David Adokuru <davidadokuru@gmail.com>
2022-06-11 09:46:14 +0100

    updates to dasboard

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -127,7 +127,7 @@
             </div>
         </div>
 		<br/><br/>
-		<div class="col-lg-12 col-md-12">
+		<div class="col-lg-12 col-md-12 mb-6">
           <div class="card">
             <div class="card-header pb-0">
               <h6>Market Data</h6>
@@ -187,41 +187,41 @@
                   <iframe scrolling="no" allowtransparency="true" frameborder="0" src="https://s.tradingview.com/embed-widget/hotlists/?locale=en#%7B%22colorTheme%22%3A%22light%22%2C%22dateRange%22%3A%2212M%22%2C%22exchange%22%3A%22US%22%2C%22showChart%22%3Atrue%2C%22largeChartUrl%22%3A%22%22%2C%22isTransparent%22%3Afalse%2C%22showSymbolLogo%22%3Afalse%2C%22width%22%3A%22100%25%22%2C%22height%22%3A%22600%22%2C%22plotLineColorGrowing%22%3A%22rgba(33%2C%20150%2C%20243%2C%201)%22%2C%22plotLineColorFalling%22%3A%22rgba(33%2C%20150%2C%20243%2C%201)%22%2C%22gridLineColor%22%3A%22rgba(240%2C%20243%2C%20250%2C%201)%22%2C%22scaleFontColor%22%3A%22rgba(120%2C%20123%2C%20134%2C%201)%22%2C%22belowLineFillColorGrowing%22%3A%22rgba(33%2C%20150%2C%20243%2C%200.12)%22%2C%22belowLineFillColorFalling%22%3A%22rgba(33%2C%20150%2C%20243%2C%200.12)%22%2C%22symbolActiveColor%22%3A%22rgba(33%2C%20150%2C%20243%2C%200.12)%22%2C%22utm_source%22%3A%22allnzassets.org%22%2C%22utm_medium%22%3A%22widget%22%2C%22utm_campaign%22%3A%22hotlists%22%7D" style="box-sizing: border-box; height: 600px; width: 100%;"></iframe>
                   
                 <style>
-	.tradingview-widget-copyright {
-		font-size: 13px !important;
-		line-height: 32px !important;
-		text-align: center !important;
-		vertical-align: middle !important;
-		/* @mixin sf-pro-display-font; */
-		font-family: -apple-system, BlinkMacSystemFont, 'Trebuchet MS', Roboto, Ubuntu, sans-serif !important;
-		color: #9db2bd !important;
-	}
+				.tradingview-widget-copyright {
+					font-size: 13px !important;
+					line-height: 32px !important;
+					text-align: center !important;
+					vertical-align: middle !important;
+					/* @mixin sf-pro-display-font; */
+					font-family: -apple-system, BlinkMacSystemFont, 'Trebuchet MS', Roboto, Ubuntu, sans-serif !important;
+					color: #9db2bd !important;
+				}
 
-	.tradingview-widget-copyright .blue-text {
-		color: #2962FF !important;
-	}
+				.tradingview-widget-copyright .blue-text {
+					color: #2962FF !important;
+				}
 
-	.tradingview-widget-copyright a {
-		text-decoration: none !important;
-		color: #9db2bd !important;
-	}
+				.tradingview-widget-copyright a {
+					text-decoration: none !important;
+					color: #9db2bd !important;
+				}
 
-	.tradingview-widget-copyright a:visited {
-		color: #9db2bd !important;
-	}
+				.tradingview-widget-copyright a:visited {
+					color: #9db2bd !important;
+				}
 
-	.tradingview-widget-copyright a:hover .blue-text {
-		color: #1E53E5 !important;
-	}
+				.tradingview-widget-copyright a:hover .blue-text {
+					color: #1E53E5 !important;
+				}
 
-	.tradingview-widget-copyright a:active .blue-text {
-		color: #1848CC !important;
-	}
+				.tradingview-widget-copyright a:active .blue-text {
+					color: #1848CC !important;
+				}
 
-	.tradingview-widget-copyright a:visited .blue-text {
-		color: #2962FF !important;
-	}
-	</style></div>
+				.tradingview-widget-copyright a:visited .blue-text {
+					color: #2962FF !important;
+				}
+				</style></div>
                 <!-- TradingView Widget END -->
             </div>
           </div>
