ded3597f1d80a38137c1eeecbc0cf09e0290b796
David Adokuru <davidadokuru@gmail.com>
2022-06-18 14:37:16 +0100

    updates

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -273,8 +273,7 @@
                                 <li>
                                        
                                         <a href="mailto:support@allianzassetshub.com">
-                                            <span class="__cf_email__" data-cfemail="cfa7aaa3a3a08fa6a1b9aee1aca0a2"><i class="fa fa-envelope" aria-hidden="true"></i>
-                                        Email: support@allianzassetshub.com</span></a>
+                                            <span class="__cf_email__" data-cfemail="cfa7aaa3a3a08fa6a1b9aee1aca0a2"><i class="fa fa-envelope" aria-hidden="true"></i>Email: support@allianzassetshub.com</span></a>
                                 </li>
                             </ul>
                         </div>
