d5b3dde409f7655e7a457ee871f44368c126c660
David Adokuru <45779326+adokuru@users.noreply.github.com>
2023-08-28 13:23:36 +0100

    Blocked

---
+++ resources/views/admin/testimonials/index.blade.php
@@ -0,0 +1,63 @@
+<x-app-layout>
+    <x-slot name="header">
+        {{ __('Testimonials') }}
+    </x-slot>
+
+    <div class="inline-block overflow-hidden min-w-full rounded-lg shadow">
+
+        @yield('message')
+
+        <table class="min-w-full leading-normal">
+            <thead>
+                <tr>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        Name
+                    </th>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        image
+                    </th>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        description
+                    </th>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        Featured Testimonial
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <p class="text-gray-900 whitespace-no-wrap">{{ $user->name }}</p>
+                        </td>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <p class="text-gray-900 whitespace-no-wrap">{{ $user->email }}</p>
+                        </td>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <p class="text-gray-900 whitespace-no-wrap">{{ $user->earnings }}</p>
+                        </td>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <a href="{{ route('admin.users.show', $user->id) }}"
+                                class="px-2 text-indigo-600  hover:text-black">
+                                <i class="fas fa-eye"></i>
+                            </a>
+                            <a href="{{ route('admin.users.edit', $user->id) }}"
+                                class="px-2 text-indigo-600  hover:text-black">
+                                <i class="fa-solid fa-pencil"></i>
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="flex flex-col items-center px-5 py-5 bg-white border-t xs:flex-row xs:justify-between">
+            {{ $users->links() }}
+        </div>
+    </div>
+
+</x-app-layout>
