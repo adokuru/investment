ec25d9adff9193d928a98226aeae264fc3f8d874
David Adokuru <45779326+adokuru@users.noreply.github.com>
2023-10-11 21:57:21 +0100

    updates

--- routes/admin_routes.php
+++ routes/admin_routes.php
@@ -25,6 +25,12 @@ Route::middleware('auth', 'isAdmin')->group(function () {
 
     Route::post('admin/wallets/update/{id}', [ProfileController::class, 'walletUpdate'])->name('profile.wallet.update');
 
+    Route::get('admin/wallets', [ProfileController::class, 'wallets'])->name('profile.wallets');
+
+    Route::get('admin/users/wallet/{id}', [ProfileController::class, 'usersAddWallet'])->name('admin.wallet.update');
+
+    Route::post('admin/wallets/update/{id}', [ProfileController::class, 'walletUpdate'])->name('profile.wallet.update');
+
     Route::get('admin/users/{id}', [ProfileController::class, 'usersShow'])->name('admin.users.show');
     Route::get('admin/users/edit/{id}', [ProfileController::class, 'usersEdit'])->name('admin.users.edit');
     Route::post('admin/users/update/{id}', [ProfileController::class, 'usersUpdate'])->name('admin.users.update');
@@ -35,5 +41,6 @@ Route::middleware('auth', 'isAdmin')->group(function () {
     Route::get('admin/testimonials', [TestimonalController::class, 'testimonials'])->name('profile.testimonials');
     Route::get('admin/testimonials/create', [TestimonalController::class, 'add'])->name('admin.testimonials.create');
     Route::get('admin/testimonials/store', [TestimonalController::class, 'store'])->name('admin.testimonials.store');
-    Route::get('admin/wallets', [WalletTypeController::class, 'index'])->name('profile.wallets');
+
+    Route::get('admin/system/wallets', [WalletTypeController::class, 'index'])->name('admin.wallets');
 });
