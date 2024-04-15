4d5bf848f10a5a21a44d1c51b7296cf3db93d4b5
David Adokuru <45779326+adokuru@users.noreply.github.com>
2024-04-15 12:39:44 +0100

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'Admin']);
-        Role::create(['name' => 'User']);
+        // Role::create(['name' => 'Admin']);
+        // Role::create(['name' => 'User']);
         $admin = User::create([
             'name' => 'Admin User',
             'email' => 'admin@allianzassetshub.com',
