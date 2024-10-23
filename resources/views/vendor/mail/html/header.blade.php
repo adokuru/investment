2aab8ec90fad4d31186b7dc6ae043d5c43d49f85
David Adokuru <45779326+adokuru@users.noreply.github.com>
2024-10-23 09:42:17 +0100

    updates

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,10 +1,10 @@
 <tr>
     <td class="header">
-        <a href="https://allnzglobalassets.org" style="display: inline-block;">
+        <a href="https://allnzonlineasset.org" style="display: inline-block;">
             @if (trim($slot) === 'Laravel')
-                <img src="https://allnzglobalassets.org/frontend-assets/img/logo.png" class="logo" alt="Logo">
+                <img src="https://allnzonlineasset.org/frontend-assets/img/logo.png" class="logo" alt="Logo">
             @else
-                <img src="https://allnzglobalassets.org/frontend-assets/img/logo.png" class="logo" alt="Logo">
+                <img src="https://allnzonlineasset.org/frontend-assets/img/logo.png" class="logo" alt="Logo">
             @endif
         </a>
     </td>
