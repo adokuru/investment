40f6aaea8c84ac9b8cf9d893d3ca2e2e4f59ea45
David Adokuru <45779326+adokuru@users.noreply.github.com>
2026-01-15 17:54:51 +0100

    updates

--- routes/auth.php
+++ routes/auth.php
@@ -17,7 +17,7 @@ Route::middleware('guest')->group(function () {
     Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('loagin');
+        ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
